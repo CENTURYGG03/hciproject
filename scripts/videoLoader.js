b904306957117dd0afcadd75f4b7e994f2c0b340
kengomatsuo <kennethfang1000@gmail.com>
2024-06-26 10:56:12 +0700

    Info and details all automated by javascript

---
+++ scripts/videoLoader.js
@@ -0,0 +1,62 @@
+window.onload = async () => {
+  const response = await fetch("../apis/videos.json")
+  const videos = await response.json()
+  if (!localStorage.getItem('current'))
+    localStorage.setItem('current', 0)
+  var current = localStorage.getItem('current')
+  console.log('hey')
+  installCurrentVideo(videos, current)
+}
+
+function installCurrentVideo(videos, current) {
+  console.log(videos[current].video)
+  document.getElementById('main-video').innerHTML = `
+  <div class="video">
+    <video controls autoplay muted>
+        <source id="main-video" src="${videos[current].video}" type="video/mp4">
+    </video>
+    <h2 class="title">Learning Addition and Subtraction - Basic Math for Kids - EASY level</h2>
+  </div>
+
+  <div class="instructor">
+    <img src="assets/img/profilepic-1.jpg" alt="profile picture">
+    <div class="profile">
+        <h3>${videos[current].teacher}</h3>
+        <h5>${videos[current].degree}</h5>
+    </div>
+    <a href="#" class="action-btn" id="see-detail-btn">Take Quiz</a>
+  </div>
+
+  <div class="desc-container">
+    <div class="desc">
+        <h4>${videos[current].views} views</h4>
+        <div class="list">
+            <p id="learning-objectives">Learning Objectives :</p>
+        </div>
+    </div>
+  </div>
+  `
+  const objectives = document.getElementById('learning-objectives')
+  videos[current].objectives.forEach(element => {
+    objectives.insertAdjacentHTML('beforeend', `
+    <li>${element}</li>
+    `)
+  });
+  const related = document.getElementById('related-videos')
+  for (let index = 0; index < videos.length; index++) {
+    if (index == current)
+      continue
+    console.log(videos[index].views)
+    related.insertAdjacentHTML('beforeend', `
+    <div class="vid" onclick="changeVideos(${index})">
+      <video muted poster="assets/img/thumbnail${index + 1}.png">
+      </video>
+      <div class="text">
+        <h2 class="title">Learning Addition and Subtraction - Basic Math for Kids - Advanced level</h2>
+        <h3>${videos[index].teacher}</h3>
+        <h5>${videos[index].views} views</h5>
+      </div>
+    </div>
+    `)
+  }
+}
