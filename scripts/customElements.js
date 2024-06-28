3b67dfba20501b922bd2e4624363a045952d7d9a
kengomatsuo <kennethfang1000@gmail.com>
2024-06-28 19:44:00 +0700

    visual fix

--- scripts/customElements.js
+++ scripts/customElements.js
@@ -1,14 +1,25 @@
+let currentPath = window.location.pathname
+currentPath = currentPath.slice(currentPath.lastIndexOf('/') + 1)
+
 class navBar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <nav class="shaded-box andika-regular">
       <a href="./index.html">Learn!</a>
-      <a href="./video.html">Learning</a>
+      <a href="./video.html">Materials</a>
       <a href="">Contact Us</a>
-      <a href="${(localStorage.getItem('auth')) ? './profile.html' : './register.html'}" id="start-button">${(localStorage.getItem('auth')) ? localStorage.getItem('username') : 'Join us'}</a>
+      <a href="${(localStorage.getItem('auth') == 'true') ? (currentPath=='profile.html') ? './index.html' : './profile.html' : './register.html'}" ${(currentPath=='profile.html') ? 'onclick="signOut()" ':''}id="start-button">${(localStorage.getItem('auth') == true) ? (currentPath=='profile.html') ? 'Log Out' : localStorage.getItem('username') : 'Join us'}</a>
     </nav>
     `
   }
 }
 
 customElements.define('navigation-bar', navBar)
+
+function signOut() {
+  localStorage.removeItem('username')
+  localStorage.removeItem('dob')
+  localStorage.removeItem('email')
+  localStorage.removeItem('gender')
+  localStorage.setItem('auth', false)
+}
